c3f0496b98648aebe894b5f4bc51d15142810119
pnareshyada <pnynaresh@gmail.com>
2022-07-28 16:42:57 +0530

    this is second commit

--- biling_details.py
+++ biling_details.py
@@ -9,18 +9,3 @@ for c in num1:
 
 
 
-Hi           
-This is Naresh Yadav working as AWS-DEVOPS Engineer for the last 3 years. I am looking for a job change. Please find the attached resume for your reference.
-
-*Total Years of Experience – 3.2 years
-*Current CTC – 720000
-*Expected CTC -1100000
-*Notice Period – less than 15 days
-*Current Location - Andrapradesh
-*Preferred Location – Bangalore
-
-
-Thanks and Regards
-
-Pathikani Naresh Yadav
-+91 9573576098
